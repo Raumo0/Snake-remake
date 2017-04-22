b59f0e92ff608bef3925769d1c09c55ace484caf
Daniil Harapko <dharapko@gmail.com>
2017-04-22 22:04:51 +0300

    Added vicious world for child enemies.

---
+++ Assets/Scripts/ViciousWorldForChilds.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViciousWorldForChilds : MonoBehaviour {
+	private Rigidbody2D rb2d;
+	private Vector3 temp;
+	private float bottom;
+	private float top;
+	private float left;
+	private float right;
+
+	public Transform meeple;
+	public GameObject grandChild;
+
+	void Start () {
+	}
+
+	void Update () {
+		bottom = GameMain.GetInstance().GetBottomBorder();
+		top = GameMain.GetInstance().GetTopBorder();
+		left = GameMain.GetInstance().GetLeftBorder();
+		right = GameMain.GetInstance().GetRightBorder();
+
+		for (int i = 0; i < this.gameObject.transform.childCount; i++) {
+			rb2d = this.gameObject.transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>();
+			if (rb2d.position.x > right) {
+				temp = rb2d.position;
+				temp.x = left;
+				rb2d.position = temp;
+			} else if (rb2d.position.x < left) {
+				temp = rb2d.position;
+				temp.x = right;
+				rb2d.position = temp;
+			}
+
+			if (rb2d.position.y > top) {
+				temp = rb2d.position;
+				temp.y = bottom;
+				rb2d.position = temp;
+			} else if (rb2d.position.y < bottom) {
+				temp = rb2d.position;
+				temp.y = top;
+				rb2d.position = temp;
+			}
+		}
+	}
+}
